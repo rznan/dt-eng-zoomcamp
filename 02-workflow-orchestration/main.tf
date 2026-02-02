a82de779ed1012a1c0f5ba2df22b8dd86381de2b
Renan <109163037+rznan@users.noreply.github.com>
2026-02-02 23:59:56 +0000

    Adds module 2

---
+++ 02-workflow-orchestration/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.0.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project
+  region  = var.gcp_location
+}
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = var.gcs_bucket_name
+  location      = var.gcp_location
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
+
+
+resource "google_bigquery_dataset" "default" {
+  dataset_id = var.bg_dataset_name
+  location = var.gcp_location
+}
---
+++ 02-workflow-orchestration/variables.tf
@@ -0,0 +1,29 @@
+variable "project" {
+  description = "My project name"
+  default     = "data-eng-zoomcamp-485022"
+}
+
+variable "gcp_location" {
+  description = "My project location"
+  default     = "southamerica-east1"
+}
+
+variable "gcp_region" {
+  description = "My project region"
+  default     = "southamerica-east1"
+}
+
+variable "bg_dataset_name" {
+  description = "My bigquery dataset name"
+  default     = "NYtaxiDataset"
+}
+
+variable "gcs_bucket_name" {
+  description = "My storage bucket name"
+  default     = "nytaxibucket"
+}
+
+variable "gcs_storage_class" {
+  description = "bucket storage class"
+  default     = "STANDARD"
+}
