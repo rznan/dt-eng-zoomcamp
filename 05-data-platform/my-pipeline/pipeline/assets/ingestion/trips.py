30ca7dcd2de021a1c786cc2a660cfd602d359ad0
Renan <109163037+rznan@users.noreply.github.com>
2026-03-11 21:44:16 +0000

    module 5 init

---
+++ 05-data-platform/my-pipeline/pipeline/assets/ingestion/trips.py
@@ -0,0 +1,47 @@
+"""@bruin
+
+# - Convention in this module: use an `ingestion.` schema for raw ingestion tables.
+name: ingestions.trips
+
+# Docs: https://getbruin.com/docs/bruin/assets/python
+type: python
+
+# Example: python:3.11
+image: python:3.11
+
+connection: duckdb-default
+
+# Bruin feature: Python materialization lets you return a DataFrame (or list[dict]) and Bruin loads it into your destination.
+# This is usually the easiest way to build ingestion assets in Bruin.
+# Alternative (advanced): you can skip Bruin Python materialization and write a "plain" Python asset that manually writes
+# into DuckDB (or another destination) using your own client library and SQL. In that case:
+# - you typically omit the `materialization:` block
+# - you do NOT need a `materialize()` function; you just run Python code
+# Docs: https://getbruin.com/docs/bruin/assets/python#materialization
+materialization:
+  type: table
+  strategy: append
+
+# Tip: mark stable identifiers as `primary_key: true` if you plan to use `merge` later.
+# Docs: https://getbruin.com/docs/bruin/assets/columns
+columns:
+  - name: pickup_datetime
+    type: timestamp
+    description: "When the meter was engaged"
+  - name: dropoff_datetime
+    type: timestamp
+    description: "When the meter was disengaged"
+
+@bruin"""
+
+import os
+import json
+import pandas as pd
+import requests
+
+def materialize():
+    start_date = os.environ["BRUIN_START_DATE"]
+    end_date = os.environ["BRUIN_END_DATE"]
+    taxi_types = json.loads(os.environ["BRUIN_VARS"]).get("taxi_types", ["yellow"])
+
+d
